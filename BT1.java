13bce108a2795d54ba478ffd7b1d8fc2bec41fb1
Phuong vy <vy002359@gmail.com>
2022-08-21 19:55:17 +0700

    HackerRank

---
+++ BT1.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class BT1 {
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+        String input=sc.nextLine();
+        sc.close();
+        System.out.println("Hello world");
+
+        System.out.println(input);
+    }
+}
